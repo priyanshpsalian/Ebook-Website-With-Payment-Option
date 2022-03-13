552f39b73d14bdb4786fa7e1e81894228c485a02
priyanshpsalian <79307288+priyanshpsalian@users.noreply.github.com>
2022-03-13 10:55:55 +0530

    uncommented app data

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const exphbs = require("express-handlebars");
 const app = express();
 
 // Handlebars Middleware
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main"} ));
 app.set("view engine", "handlebars");
 
 // Body Parser Middleware
@@ -23,28 +23,28 @@ app.use(express.static(`${__dirname}/public`));
 app.get("/", (req, res) => {
   res.render("index");
 });
-// app.get("/", (req, res) => {
-//   res.render("index", {
-//     stripePublishableKey: keys.stripePublishableKey,
-//   });
-// });
+app.get("/", (req, res) => {
+  res.render("index", {
+    stripePublishableKey: keys.stripePublishableKey,
+  });
+});
 
 // Charge Route
-// app.post('/charge', (req, res) => {
-//   const amount = 2500;
+app.post('/charge', (req, res) => {
+  const amount = 2500;
 
-//   stripe.customers.create({
-//     email: req.body.stripeEmail,
-//     source: req.body.stripeToken
-//   })
-//   .then(customer => stripe.charges.create({
-//     amount,
-//     description: 'Web Development Ebook',
-//     currency: 'usd',
-//     customer: customer.id
-//   }))
-//   .then(charge => res.render('success'));
-// });
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount,
+    description: 'Web Development Ebook',
+    currency: 'usd',
+    customer: customer.id
+  }))
+  .then(charge => res.render('success'));
+});
 
 const port = process.env.PORT || 5000;
 
