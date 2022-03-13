e3b7377af3156526e1e474fcd154ef46faa05455
priyanshpsalian <79307288+priyanshpsalian@users.noreply.github.com>
2022-03-13 10:49:12 +0530

    first

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+// const keys = require("./config/keys");
+const stripe = require("stripe")(
+  "sk_test_51KBMLXSCe6a9yT31WMxyDy7Ei5na3uqe5iMp24MNweCyIpGL5cZpIdbwlJu0dKehrySrTv8ue5zaCrHlI3NfS8N8004NuPSaqV"
+);
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+// app.get("/", (req, res) => {
+//   res.render("index", {
+//     stripePublishableKey: keys.stripePublishableKey,
+//   });
+// });
+
+// Charge Route
+// app.post('/charge', (req, res) => {
+//   const amount = 2500;
+
+//   stripe.customers.create({
+//     email: req.body.stripeEmail,
+//     source: req.body.stripeToken
+//   })
+//   .then(customer => stripe.charges.create({
+//     amount,
+//     description: 'Web Development Ebook',
+//     currency: 'usd',
+//     customer: customer.id
+//   }))
+//   .then(charge => res.render('success'));
+// });
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
